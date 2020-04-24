920ec94a5d33cff6284f864af7de0d22cfea3b5a
Giuliano Procida <gprocida@google.com>
2020-04-24 11:04:28 +0100

    Add suppression change kind parsing error checks.
    This commit moves and adjusts existng parse routines for change kinds,
    adding error checking in the process. The default change kind value in
    the suppression types is ALL_CHANGE_KINDS. The now superfluous
    UNDEFINED_CHANGE_KIND enumerator is removed.
    
    Behavioural changes are as follows:
    
    - In the case of an misspelt change kind, the code would have
      previously silently set the change kind to UNDEFINED_CHANGE_KIND (a
      bitset of 0) which in turn would have neutralised the containing
      suppression.
    - Following this change, the code will now treat this as an error,
      behave as if no value were supplied and default to ALL_CHANGE_KINDS.
    
    Placeholders have been added for error reporting.
    
    	* include/abg-suppression.h
    	(function_suppression::change_kind): Remove
    	UNDEFINED_CHANGE_KIND.
    	(function_suppression::parse_change_kind): Drop this.
    	(variable_suppression::change_kind): Remove
    	UNDEFINED_CHANGE_KIND.
    	(variable_suppression::parse_change_kind): Drop this.
    	* src/abg-suppression.cc (string_to_function_change_kind): New
    	function based on old function_suppression::parse_change_kind
    	method, with error checking.
    	(string_to_variable_change_kind): New variable based on old
    	variable_suppression::parse_change_kind method, with error
    	checking. (read) New overloads for
    	function_suppression::change_kind and
    	variable_suppression::change_kind values.
    	(function_suppression::parse_change_kind): Drop this.
    	(variable_suppression::parse_change_kind): Drop this.
    
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- include/abg-suppression.h
+++ include/abg-suppression.h
@@ -395,7 +395,6 @@ public:
   /// to.
   enum change_kind
   {
-    UNDEFINED_CHANGE_KIND,
     /// A change in a sub-type of the function.
     FUNCTION_SUBTYPE_CHANGE_KIND = 1,
     /// The function was added to the second subject of the diff.
@@ -413,9 +412,6 @@ public:
   function_suppression();
 
   virtual ~function_suppression();
-
-  static change_kind
-  parse_change_kind(const string&);
 
   change_kind
   get_change_kind() const;
@@ -592,7 +588,6 @@ public:
   /// to.
   enum change_kind
   {
-    UNDEFINED_CHANGE_KIND,
     /// A change in a sub-type of the variable.
     VARIABLE_SUBTYPE_CHANGE_KIND = 1,
     /// The variable was added to the second second subject of the
@@ -617,9 +612,6 @@ public:
   variable_suppression();
 
   virtual ~variable_suppression();
-
-  static change_kind
-  parse_change_kind(const string&);
 
   change_kind
   get_change_kind() const;
