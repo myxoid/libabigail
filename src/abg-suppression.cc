04d48704c37ba26e86cb296143c296c871ec6822
Giuliano Procida <gprocida@google.com>
2020-04-28 10:48:26 +0100

    Add string_to_parameter_spec error reporting.
    This commit ensures error messages are emitted when parsing fails.
    
    	* src/abg-suppression.cc (string_to_parameter_spec): Replace
    	placeholders for error reporting with actual error reporting.
    
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- src/abg-suppression.cc
+++ src/abg-suppression.cc
@@ -213,19 +213,19 @@ string_to_parameter_spec(const std::string& str,
 
   if (need_slash)
     {
-      // TODO: maybe emit missing trailing '/' message
+      std::cerr << "error: missing trailing '/' in '" << str << "'\n";
       return false;
     }
 
   if (cur != str.size())
     {
-      // TODO: maybe emit trailing junk message
+      std::cerr << "error: trailing junk in '" << str << "'\n";
       return false;
     }
 
   if (index < 0 && !is_regex && type_name.empty())
     {
-      // TODO maybe emit bad parameter specification message
+      std::cerr << "error: bad parameter specification '" << str << "'\n";
       return false;
     }
 
@@ -235,7 +235,7 @@ string_to_parameter_spec(const std::string& str,
       type_name_regex = regex::compile(type_name);
       if (!type_name_regex)
 	{
-	  // TODO: maybe emit bad regex message
+	  std::cerr << "warning: bad regex '" << type_name << "'\n";
 	  return false;
 	}
       type_name.clear();
