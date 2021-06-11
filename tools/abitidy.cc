msft-mirror-aosp/platform.external.libabigail

abitidy: drop empty elements
Certain elements in ABI XML are effectvely containers whose attributes
do not carry ABI information. They can be dropped if empty.

- elf-variable-symbols: pure container
- elf-function-symbols: pure container
- namespace-decl: C++ namespace (has a name attribute)
- abi-instr: compilation unit (has path etc.)
- abi-corpus: binary object (has architecture)
- abi-corpus-group: binary objects (has architecture)

It could be argued that abi-corpus (or abi-corpus-group) should be
kept around to hold the architecture of an object or set of objects.
However, if a binary object has no symbols (say, if it is empty), it
hardly matters what the architecture is.

Empty elements can also be produced during manipulation of the XML.

Note that abidiff currently rejects completely empty files so we avoid
dropping empty top-level elements.

	* tools/abitidy.cc (from_libxml): Cast a libxml string to C
	string.
	(remove_element): Remove an XML element and any immediately
	preceding comment.
	(DROP_IF_EMPTY): Set of names of elements that are safe to
	drop if empty.
	(drop_empty): Drop empty elements, if safe to do so,
	recursively.
	(main): Add drop-empty option processing and action.

Bug: 181269148
Change-Id: Ie728522026210d48a49da99bbd453c0be8139721
Signed-off-by: Giuliano Procida <gprocida@google.com>

tools/abitidy.cc
@@ -42,6 +42,17 @@ to_libxml(const char* str)
   return reinterpret_cast<const xmlChar*>(str);
 }
 
+/// Cast a libxml string to C string.
+///
+/// @param str the libxml string (pointer)
+///
+/// @return the same thing, as a type compatible with the C library API
+static const char*
+from_libxml(const xmlChar* str)
+{
+  return reinterpret_cast<const char*>(str);
+}
+
 /// Remove a node from its document and free its storage.
 ///
 /// @param node the node to remove
@@ -50,6 +61,18 @@ remove_node(xmlNodePtr node)
 {
   xmlUnlinkNode(node);
   xmlFreeNode(node);
+}
+
+/// Remove an XML element and any immediately preceding comment.
+///
+/// @param node the element to remove
+static void
+remove_element(xmlNodePtr node)
+{
+  xmlNodePtr previous_node = node->prev;
+  if (previous_node && previous_node->type == XML_COMMENT_NODE)
+    remove_node(previous_node);
+  remove_node(node);
 }
 
 /// Get child nodes of given node.
@@ -189,6 +212,32 @@ adjust_quotes(xmlChar* start, xmlChar* limit)
     }
 }
 
+static const std::set<std::string> DROP_IF_EMPTY = {
+  "elf-variable-symbols",
+  "elf-function-symbols",
+  "namespace-decl",
+  "abi-instr",
+  "abi-corpus",
+  "abi-corpus-group",
+};
+
+/// Drop empty elements, if safe to do so, recursively.
+///
+/// @param element node to process
+static void
+drop_empty(xmlNodePtr node)
+{
+  if (node->type != XML_ELEMENT_NODE)
+    return;
+  for (xmlNodePtr child : get_children(node))
+    drop_empty(child);
+  // Do not drop the root element, even if empty.
+  if (node->parent->type == XML_DOCUMENT_NODE)
+    return;
+  if (!node->children && DROP_IF_EMPTY.count(from_libxml(node->name)))
+    remove_element(node);
+}
+
 /// Main program.
 ///
 /// Read and write ABI XML, with optional extra processing passes.
@@ -205,6 +254,7 @@ main(int argc, char* argv[])
   const char* opt_input = NULL;
   const char* opt_output = NULL;
   int opt_indentation = 2;
+  bool opt_drop_empty = false;
 
   // Process command line.
   auto usage = [&]() -> int {
@@ -213,6 +263,7 @@ main(int argc, char* argv[])
               << " [-o|--output file]"
               << " [-I|--indentation n]"
               << " [-a|--all]"
+              << " [-d|--[no-]drop-empty]"
               << '\n';
     return 1;
   };
@@ -237,7 +288,11 @@ main(int argc, char* argv[])
             exit(usage());
         }
       else if (!strcmp(arg, "-a") || !strcmp(arg, "--all"))
-        ;
+        opt_drop_empty = true;
+      else if (!strcmp(arg, "-d") || !strcmp(arg, "--drop-empty"))
+        opt_drop_empty = true;
+      else if (!strcmp(arg, "--no-drop-empty"))
+        opt_drop_empty = false;
       else
         exit(usage());
     }
@@ -269,6 +324,11 @@ main(int argc, char* argv[])
   // Strip text nodes to simplify other operations.
   for (xmlNodePtr node = document->children; node; node = node->next)
     strip_text(node);
+
+  // Drop empty elements.
+  if (opt_drop_empty)
+    for (xmlNodePtr node = document->children; node; node = node->next)
+      drop_empty(node);
 
   // Reformat XML for human consumption.
   for (xmlNodePtr node = document->children; node; node = node->next)

myxoid/libabigail

tools/abitidy.cc
@@ -139,11 +139,36 @@ void format_xml(size_t indent, xmlNodePtr node)
     }
 }
 
+static const std::unordered_set<std::string> drop_if_empty = {
+  "elf-variable-symbols",
+  "elf-function-symbols",
+  "namespace-decl",
+  "abi-instr",
+  "abi-corpus",
+  "abi-corpus-group",
+};
+
+// Drop empty elements, if safe to do so, recursively.
+//
+// Args:
+//   node
+//
+void drop_empty(xmlNodePtr node)
+{
+  for (xmlNodePtr child : get_children(node))
+    drop_empty(child);
+  if (!node->children && node->type == XML_ELEMENT_NODE && drop_if_empty.count(from_libxml(node->name)))
+    // Until abidiff accepts empty ABIs, avoid dropping top-level elements.
+    if (node->parent->type == XML_ELEMENT_NODE)
+      remove_node(node);
+}
+
 int main(int argc, char * argv[])
 {
   // Defaults.
   const char * opt_in = NULL;
   const char * opt_out = NULL;
+  bool opt_drop_empty = false;
 
   // Process command line.
   auto usage = [&]() -> int {
@@ -151,6 +176,7 @@ int main(int argc, char * argv[])
               << " [-i|--input file]"
               << " [-o|--output file]"
               << " [-a|--all]"
+              << " [-d|--[no-]drop-empty]"
               << '\n';
     return 1;
   };
@@ -168,7 +194,11 @@ int main(int argc, char * argv[])
       else if (!strcmp(arg, "-o") || !strcmp(arg, "--output"))
         opt_out = get_arg();
       else if (!strcmp(arg, "-a") || !strcmp(arg, "--all"))
-        ;
+        opt_drop_empty = true;
+      else if (!strcmp(arg, "-d") || !strcmp(arg, "--drop-empty"))
+        opt_drop_empty = true;
+      else if (!strcmp(arg, "--no-drop-empty"))
+        opt_drop_empty = false;
       else
         exit(usage());
     }
@@ -192,6 +222,11 @@ int main(int argc, char * argv[])
 
   // Strip text nodes to simplify other operations.
   strip_text(doc);
+
+  // Drop empty elements.
+  if (opt_drop_empty)
+    for (xmlNodePtr node = doc->children; node; node = node->next)
+      drop_empty(node);
 
   // Reformat XML for human consumption.
   for (xmlNodePtr node = doc->children; node; node = node->next)